f83ec054dc2dcb05942e5163e4c2af0783c02328
daalekz <dontdissthesonic123@gmail.com>
2021-03-10 02:41:24 +1100

    improved logging

--- bot.js
+++ bot.js
@@ -1,67 +1,73 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable no-var */
 const Discord = require('discord.js');
 const _ = require('underscore');
 const config = require('./config/config.js');
+const date = require('date-and-time');
+const fs = require('fs');
 const client = new Discord.Client();
-// const config = require('./bot-config.js');
+const PastebinAPI = require('pastebin-ts');
+var log = null;
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}`);
+	// setting up logging
+	openLog();
+	appendLine('------- Warming up -------');
+	appendLine(`Logged in as ${client.user.tag}`);
 	const guilds = client.guilds.cache.map(guild => guild.name);
-	console.log(guilds);
-
+	appendLine(guilds);
+	appendLine('------- Finished warmup -------');
 });
 
 client.on('voiceStateUpdate', function(oldVoiceState, newVoiceState) {
-	console.log(`Voice state of ${oldVoiceState.member.user.username} has changed in the following ways:`);
-	// console.log(diff(newVoiceState, oldVoiceState));
 	var newUserChannel = newVoiceState.channelID;
 	var oldUserChannel = oldVoiceState.channelID;
 
 	if (newUserChannel && oldUserChannel == null) {
-		console.log('i think is join');
+		appendLine(`${oldVoiceState.member.user.username} has joined ${newVoiceState.channel.name}`);
 		if (newVoiceState.channel.parent && newVoiceState.channel.parent.id == config.channelCategory) {
 			oldVoiceState.member.roles.add([config.inVoiceRole]).catch(console.error);
 		}
 	}
 	else if (newUserChannel && oldUserChannel && oldUserChannel != newUserChannel) {
-		console.log('i think is move channels');
+		appendLine(`${oldVoiceState.member.user.username} has moved from ${oldVoiceState.channel.name} to ${newVoiceState.channel.name}`);
 		if(newVoiceState.channel.parent && newVoiceState.channel.parent.id != config.channelCategory) {
 			oldVoiceState.member.roles.remove([config.inVoiceRole]).catch(console.error);
-			console.log('i think is move out of category?');
+			appendLine('-- not in category');
 		}
 		if(newVoiceState.channel.parent && newVoiceState.channel.parent.id == config.channelCategory) {
 			oldVoiceState.member.roles.add([config.inVoiceRole]).catch(console.error);
-			console.log('i think is move within category?');
 		}
 	}
 	else if (newUserChannel == null && oldUserChannel != null) {
-		console.log('i think is leave');
+		appendLine(`${oldVoiceState.member.user.username} has left ${oldVoiceState.channel.name}`);
 		oldVoiceState.member.roles.remove([config.inVoiceRole]).catch(console.error);
 	}
 	else {
-		console.log('i think is something else (mute/unmute/share, etc.)');
+		appendLine('i think is something else (mute/unmute/share, etc.)');
 	}
 });
 
 client.on('message', function(message) {
-	if (message.content === '.wipe') {
+	if (message.content === '.wipe' && message.channel == config.inVoiceTextChannel) {
 		wipe(client.channels.cache.get(config.inVoiceTextChannel));
-		console.log(`channel wiped by ${message.author.username}`);
+		appendLine(`channel wiped by ${message.author.username} at ${message.createdAt}`);
+		message.channel.send('Welcome to the In-Voice text channel. You may use the `.wipe` command to wipe the contents of this channel at any time. Further improvements to the bot (including auto-wiping on empty voice channels) will be added progressively. ');
+		return;
+	}
+	// graceful shutdown - admin only
+	if (message.content === '.shutdown' && message.member.hasPermission('ADMINISTRATOR')) {
+		log.end();
+		client.destroy();
+	}
+	if (message.content === '.logs' && message.member.hasPermission('ADMINISTRATOR')) {
+		log.end();
+		uploadLogs(message);
+		openLog();
+		appendLine(`Logs requested by ${message.member.user.username}`);
 	}
 });
 
-function diff(a, b) {
-	var r = {};
-	_.each(a, function(v, k) {
-		if(b[k] === v) return;
-		r[k] = _.isObject(v)
-			? _.diff(v, b[k])
-			: v
-		;
-	});
-	return r;
-}
 async function wipe(channel) {
 	var msg_size = 100;
 	while (msg_size == 100) {
@@ -69,5 +75,37 @@ async function wipe(channel) {
 			.then(messages => msg_size = messages.size)
 			.catch(console.error);
 	}
+}
+
+function appendLine(message, timestamp = date.format(new Date(), 'ddd DD MMM YYYY hh:mm:ss')) {
+	log.write(`${timestamp}: ${message}\n`);
+	console.log(message);
+}
+
+function uploadLogs(message) {
+	// 0=public, 1=unlisted, 2=private
+	const privateValue = 1;
+	const pastebin = new PastebinAPI({
+		'api_dev_key' : config.pastebinAPIKey,
+		'api_user_name' : config.pastebinUsername,
+		'api_user_password' : config.pastebinPassword,
+		'api_paste_private' : privateValue
+	});
+	pastebin.createPasteFromFile({
+		'file': config.logFilePath,
+		'title': `logs ${getdateTime()}`
+	})
+		.then((data) => {
+			message.reply(`your logs are available here: ${data}`);
+		})
+		.catch((err) => {
+			appendLine(`Error in hastebin upload: ${err}`);
+		});
+}
+function openLog() {
+	log = fs.createWriteStream(config.logFilePath, { flags: 'a' });
+}
+function getdateTime() {
+	return date.format(new Date(), 'ddd DD MMM YYYY hh:mm:ss');
 }
 client.login(config.botToken);
