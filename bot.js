fcda96805a2a079ba24248a08ea411e7d326d6c1
daalekz <dontdissthesonic123@gmail.com>
2022-01-04 00:28:26 +1100

    Revert "wip feat: reminders; persistence"
    This reverts commit 55d7f630c888bb70c89be3de76a7c5d6e1fae160.

--- bot.js
+++ bot.js
@@ -1,14 +1,12 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-var */
 const Discord = require('discord.js');
 const config = require('./config/config.js');
 const date = require('date-and-time');
 const fs = require('fs');
-const mysql = require('mysql');
-// const scheduler = require('node-schedule');
-
 const client = new Discord.Client();
-let sysLog = null;
-let spotifyLog = null;
-let db = null;
+var sysLog = null;
+var spotifyLog = null;
 
 client.on('ready', () => {
 	// setting up logging
@@ -17,40 +15,26 @@ client.on('ready', () => {
 	appendSysLog(`Logged in as ${client.user.tag}`);
 	const guilds = client.guilds.cache.map(guild => guild.name);
 	appendSysLog(guilds);
-	appendSysLog('Connecting to sql...');
-
-	db = mysql.createConnection({
-		host: config.dbCredentials.host,
-		user: config.dbCredentials.user,
-		password: config.dbCredentials.password,
-		database: config.dbCredentials.database,
-		port: config.dbCredentials.port,
-	});
-
-	db.connect(function(err) {
-		if (err) throw err;
-		appendSysLog('Connected sql!');
-		appendSysLog('------- Finished warmup -------');
-	});
+	appendSysLog('------- Finished warmup -------');
 });
 
 client.on('voiceStateUpdate', function(oldVoiceState, newVoiceState) {
-	const newUserChannel = newVoiceState.channelID;
-	const oldUserChannel = oldVoiceState.channelID;
+	var newUserChannel = newVoiceState.channelID;
+	var oldUserChannel = oldVoiceState.channelID;
 
 	if (newUserChannel && oldUserChannel == null) {
 		appendSysLog(`${oldVoiceState.member.user.username} has joined ${newVoiceState.channel.name}`);
-		if (newVoiceState.channel.parent && newVoiceState.channel.parent.id === config.channelCategory) {
+		if (newVoiceState.channel.parent && newVoiceState.channel.parent.id == config.channelCategory) {
 			oldVoiceState.member.roles.add([config.inVoiceRole]).catch(console.error);
 		}
 	}
-	else if (newUserChannel && oldUserChannel && oldUserChannel !== newUserChannel) {
+	else if (newUserChannel && oldUserChannel && oldUserChannel != newUserChannel) {
 		appendSysLog(`${oldVoiceState.member.user.username} has moved from ${oldVoiceState.channel.name} to ${newVoiceState.channel.name}`);
-		if(newVoiceState.channel.parent && newVoiceState.channel.parent.id !== config.channelCategory) {
+		if(newVoiceState.channel.parent && newVoiceState.channel.parent.id != config.channelCategory) {
 			oldVoiceState.member.roles.remove([config.inVoiceRole]).catch(console.error);
 			appendSysLog('-- not in category');
 		}
-		if(newVoiceState.channel.parent && newVoiceState.channel.parent.id === config.channelCategory) {
+		if(newVoiceState.channel.parent && newVoiceState.channel.parent.id == config.channelCategory) {
 			oldVoiceState.member.roles.add([config.inVoiceRole]).catch(console.error);
 		}
 	}
@@ -63,67 +47,39 @@ client.on('voiceStateUpdate', function(oldVoiceState, newVoiceState) {
 	}
 });
 
-function persistReminders(reminders) {
-	const query = `INSERT INTO ${config.dbCredentials.database}.${config.dbCredentials.table} (triggerDate, targetUser, guild, msgUrl, dm, msgSummary) VALUES ?`;
-	db.query(query, [reminders], function(err, result) {
-		if (err) throw err;
-		console.log(`${result.affectedRows} reminder(s) inserted`);
-	});
-}
-
 client.on('message', function(message) {
-	if (message.content === '.wipe' && message.channel === config.inVoiceTextChannel) {
+	if (message.content === '.wipe' && message.channel == config.inVoiceTextChannel) {
 		wipe(client.channels.cache.get(config.inVoiceTextChannel));
 		appendSysLog(`channel wiped by ${message.author.username} at ${message.createdAt}`);
 		message.channel.send('Welcome to the In-Voice text channel. You may use the `.wipe` command to wipe the contents of this channel at any time. Further improvements to the bot (including auto-wiping on empty voice channels) will be added progressively. ');
 		return;
 	}
 	if (message.content === '.wipe' && message.member.hasPermission('ADMINISTRATOR')) {
-		wipe(message.channel).then(() => {
-			appendSysLog(`channel ${message.channel.name} wiped by ${message.author.username} at ${message.createdAt}`);
-		}).then(() => {
-			return;
-		});
-	}
-	if (message.content.startsWith('.remindme')) {
-		const userDelta = getTimeFromMessage(message.content);
-
-		// if its not a valid input, get out
-		if (userDelta === null) {
-			message.reply('not a valid remindme :/').catch(console.error);
-			return;
-		}
-
-		// add the time period to the current date to get our reminder time
-		const reminderDate = new Date(Date.now() + userDelta);
-		const reminderRow = [[reminderDate, message.author.id, message.guild.id, message.url, 0, message.content.substring(0, 100)]];
-		persistReminders(reminderRow);
-		message.reply(reminderDate.toString()).catch(console.error);
-
+		wipe(message.channel);
+		appendSysLog(`channel ${message.channel.name} wiped by ${message.author.username} at ${message.createdAt}`);
+		return;
 	}
 	// graceful shutdown - admin only
-	if (message.content === '..shutdown' && message.member.hasPermission('ADMINISTRATOR')) {
+	if (message.content === '.shutdown' && message.member.hasPermission('ADMINISTRATOR')) {
 		message.react('👋').then (() => {
 			sysLog.end();
 			spotifyLog.end();
 			client.destroy();
-			process.exit(0);
+			return;
 		});
 	}
 	if (message.channel == config.bopOfTheDayChannel) {
-		tryParseSongIntoList(message).then(() => {
-			message.react('✅');
-		});
+		tryParseSongIntoList(message);
 		return;
 	}
 });
 
 async function wipe(channel) {
-	let msg_size = 100;
+	var msg_size = 100;
 	do {
 		console.log(msg_size);
-		const fetched = await channel.messages.fetch({ limit: 100 });
-		const notPinned = fetched.filter(fetchedMsg => !fetchedMsg.pinned);
+		var fetched = await channel.messages.fetch({ limit: 100 });
+		var notPinned = fetched.filter(fetchedMsg => !fetchedMsg.pinned);
 		await channel.bulkDelete(notPinned, true)
 			.then(messages => msg_size = messages.size)
 			.catch(console.error);
@@ -131,15 +87,16 @@ async function wipe(channel) {
 }
 
 async function tryParseSongIntoList(message) {
-	const messageContent = message.content;
+	var messageContent = message.content;
 	// eslint-disable-next-line no-useless-escape
 	const regex = '(https:\/\/open.spotify.com\/track\/[A-Za-z0-9]{22})';
-	const found = messageContent.match(regex);
+	var found = messageContent.match(regex);
 	const uniqueSongs = [... new Set(found)];
 	if (uniqueSongs.length > 0) {
 		uniqueSongs.forEach((element) => {
 			appendSpotifyLog(element);
 		});
+		message.react('✅');
 	}
 }
 
@@ -148,79 +105,13 @@ function appendSysLog(message, timestamp = date.format(new Date(), 'ddd DD MMM Y
 	console.log(message);
 }
 
-async function appendSpotifyLog(song) {
-	await spotifyLog.write(`${song}\n`);
+function appendSpotifyLog(song) {
+	spotifyLog.write(`${song}\n`);
 }
 
 function openLog() {
 	sysLog = fs.createWriteStream(config.logFilePath, { flags: 'a' });
 	spotifyLog = fs.createWriteStream(config.logFilePathSpotify, { flags: 'a' });
-}
-
-/**
-	 Converts the user's message containing length of time information into a number of ms
-	@param {String} content The remindme command message string
-	@returns {Number} A number in milliseconds of how much time was supplied
- **/
-function getTimeFromMessage(content) {
-	const regex = /\b(?<num>\d+(.\d+)?) ?(?<length>(y(ears?)?)|(mon(th)?s?)|(w(eeks?)?)|(d(ays?)?)|(h(ours?)?)|(m(in(ute)?s?)?)|(s(ec(ond)?s?)?))\b/gm;
-	if(!regex.test(content)) {
-		return null;
-	}
-	// reset regex position
-	regex.lastIndex = 0;
-
-	// all possible accepted variations on time lengths
-	const validTimes = {
-		seconds: ['s', 'sec', 'secs', 'second', 'seconds'],
-		minutes: ['m', 'min', 'mins', 'minute', 'minutes'],
-		hours: ['h', 'hour', 'hours'],
-		days: ['d', 'day', 'days'],
-		years: ['y', 'year', 'years'],
-	};
-
-	let match = null;
-	const timeObj = {};
-	try{
-		while(match = regex.exec(content)) {
-			// find full version of time length string
-			let length = null;
-			for(const time in validTimes) {
-				if(validTimes[time].includes(match.groups.length)) length = time;
-			}
-
-			// check if property is duplicate
-			if(timeObj[length]) throw new Error(`cannot have multiple values for time length ${length}`);
-
-			// add duration to length property
-			timeObj[length] = parseFloat(match.groups.num);
-		}
-	}
-	catch(err) {
-		return null;
-	}
-	return convertToMs(timeObj);
-}
-
-// Yoinked from SleepBotExe (cheers)
-/**
- * Converts an Object containing length of time information into a number of ms
- *
- * @param {Object} timeObj An Object containing information about the length of time to wait
- * @returns {Number} A number in milliseconds of how much time was supplied
- */
-function convertToMs(timeObj) {
-	let output = 0;
-
-	if(timeObj.years) output += parseInt(timeObj.years * 1000 * 60 * 60 * 24 * 365);
-	if(timeObj.months) output += parseInt(timeObj.months * 1000 * 60 * 60 * 24 * 31);
-	if(timeObj.weeks) output += parseInt(timeObj.weeks * 1000 * 60 * 60 * 24 * 7);
-	if(timeObj.days) output += parseInt(timeObj.days * 1000 * 60 * 60 * 24);
-	if(timeObj.hours) output += parseInt(timeObj.hours * 1000 * 60 * 60);
-	if(timeObj.minutes) output += parseInt(timeObj.minutes * 1000 * 60);
-	if(timeObj.seconds) output += parseInt(timeObj.seconds * 1000);
-
-	return output;
 }
 
 client.login(config.botToken);
