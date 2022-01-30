ef3ef5be8b61cb6649726f05464b4cc8acf1d22b
daalekz <dontdissthesonic123@gmail.com>
2022-01-30 13:51:13 +1100

    wip

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const Discord = require('discord.js'),
 	path = require('path'),
 	fs = require('fs'),
 	{ appendSysLog } = require('./utils.js'),
+	wordle = require('./wordle.js'),
 	client = new Discord.Client();
 
 client.prefix = config.prefix;
@@ -58,9 +59,9 @@ client.on('message', function(message) {
 		return;
 	}
 	if (message.channel == config.wordleChannel) {
-		var result = tryParseWordle(message);
+		var result = wordle.tryParseWordle(message);
 		if (result) {
-			tryAddWordleEntry(result);
+			wordle.tryAddWordleEntry(result);
 			message.react('✅');
 		} else {
 			message.reply('You\'ve already sent one today...');
@@ -123,67 +124,7 @@ client.on('message', function(message) {
 	}
 });
 
-async function tryParseWordle(message) {
-	var messageContent = message.content;
-	// eslint-disable-next-line no-useless-escape
-	const regex = /^Wordle\s\d\d\d\s.\/\d/m;
-	if (regex.test(message.content)) {
-		const split = messageContent.split(' ', 3);
-		const attempt = split[2].substring(0, 1);
-		var wordleEntry = {
-			timeStamp: Date.now(),
-			compositeKey : split[1] + '_' + message.author.id,
-			id : split[1],
-			submitterId : message.author.id,
-			attempt : attempt,
-			isHardMode : split[2].substring(3, 4) == '*',
-			success : attempt != 'X',
-		};
-		//todo better defensive coding because alex is an idiot --
-		// ie reasonable no. guesses, confirm that it is actually today's wordle, etc. 
-		return wordleEntry;
-	}
-	return null;
-}
 
-function tryAddWordleEntry(entry) {
-// retrieve, deserialise, append, reserialise, persist
-	var data = JSON.parse(fs.readFileSync('./wordle.json'));
-	// data integrity checks --if someone's already submitted we're not going to let them do it again.
-
-	const found = data.entries.some(el => el.compositeKey === entry.compositeKey);
-	if (found) {
-		return false;
-	}
-	data.entries.push(entry);
-	var updatedData = JSON.stringify(data);
-	fs.writeFileSync('./wordle.json', updatedData);
-	return true;
-}
-
-//gets the score for a user (total capture period possible guesses - how many they took)
-async function getWordleTotalGuesses(authorId) {
-	var data = JSON.parse(fs.readFileSync('./wordle.json'));
-	data.entries.filter(entry=>entry.authorId == authorId)
-}
-async function getNumDistinctWordles() {
-	var data = JSON.parse(fs.readFileSync('./wordle.json'));
-	const wordlesPossibleDuringCapturePeriod = data.hasMax(id) - data.hasMin(id);
-	return wordlesPossibleDuringCapturePeriod;
-}
-function getNumPossibleAttempts(numWordle) {
-	return numwordle * 6;
-}
-Array.prototype.hasMin = function(attrib) {
-	return (this.length && this.reduce(function(prev, curr){ 
-		return prev[attrib] < curr[attrib] ? prev : curr; 
-	})) || null;
- }
- Array.prototype.hasMax = function(attrib) {
-	return (this.length && this.reduce(function(prev, curr){ 
-		return prev[attrib] > curr[attrib] ? prev : curr; 
-	})) || null;
- }
 
 async function tryParseSongIntoList(message) {
 	var messageContent = message.content;
---
+++ wordle.js
@@ -0,0 +1,75 @@
+async function tryParseWordle(message) {
+	var messageContent = message.content;
+	// eslint-disable-next-line no-useless-escape
+	const regex = /^Wordle\s\d\d\d\s.\/\d/m;
+	if (regex.test(message.content)) {
+		const split = messageContent.split(' ', 3);
+		const attempt = split[2].substring(0, 1);
+		var wordleEntry = {
+			timeStamp: Date.now(),
+			compositeKey: split[1] + '_' + message.author.id,
+			id: split[1],
+			submitterId: message.author.id,
+			attempt: attempt,
+			isHardMode: split[2].substring(3, 4) == '*',
+			success: attempt != 'X',
+		};
+		return validateWordleObj(wordleEntry) ? wordleEntry : null;
+	}
+	return null;
+}
+
+function validateWordleObj(entry) {
+	//reasonable attempt number
+	if (entry.success) {
+		return (entry.attempt >= 1 && entry.attempt <= 6);
+	}
+	//is from the last 2 days of wordles
+	var validationDate = Date.now.days() - entry.id;
+	const wordleAbsoluteStartDate = Date.parse('19-6-2021');
+	if (validationDate - wordleAbsoluteStartDate <= 2) return true;
+}
+
+function tryAddWordleEntry(entry) {
+	// retrieve, deserialise, append, reserialise, persist
+	var data = JSON.parse(fs.readFileSync('./wordle.json'));
+	// data integrity checks --if someone's already submitted we're not going to let them do it again.
+
+	const found = data.entries.some(el => el.compositeKey === entry.compositeKey);
+	if (found) {
+		return false;
+	}
+	data.entries.push(entry);
+	var updatedData = JSON.stringify(data);
+	fs.writeFileSync('./wordle.json', updatedData);
+	return true;
+}
+
+//gets the score for a user (total capture period possible guesses - how many they took)
+//further aspects to account for:
+// streaks - get today then while decrement id has value && success, add to counter 
+// outperforming the pack or better than the average. 
+// hardmode = 5% bonus
+
+async function getWordleTotalGuesses(authorId) {
+	var data = JSON.parse(fs.readFileSync('./wordle.json'));
+	data.entries.filter(entry => entry.authorId == authorId)
+}
+async function getNumDistinctWordles() {
+	var data = JSON.parse(fs.readFileSync('./wordle.json'));
+	const wordlesPossibleDuringCapturePeriod = data.hasMax(id) - data.hasMin(id);
+	return wordlesPossibleDuringCapturePeriod;
+}
+function getNumPossibleAttempts(numWordle) {
+	return numwordle * 6;
+}
+Array.prototype.hasMin = function (attrib) {
+	return (this.length && this.reduce(function (prev, curr) {
+		return prev[attrib] < curr[attrib] ? prev : curr;
+	})) || null;
+}
+Array.prototype.hasMax = function (attrib) {
+	return (this.length && this.reduce(function (prev, curr) {
+		return prev[attrib] > curr[attrib] ? prev : curr;
+	})) || null;
+}
