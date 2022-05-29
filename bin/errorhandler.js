13cb74b27ee4ef7507b4f488697eef9c6354165d
daalekz <dontdissthesonic123@gmail.com>
2022-05-29 13:56:47 +1000

    lint fixes

--- bin/errorhandler.js
+++ bin/errorhandler.js
@@ -3,22 +3,22 @@ const Discord 		= require('discord.js');
 module.exports = {
 	/**
 	 * Sends a message that indicates an error has occurred.
-	 * 
+	 *
 	 * @param {Discord.Message} message The original message which caused the Error
 	 * @param {Error} err The Error object caught by the program
 	 * @returns {Promise<Discord.Message>} The message sent to a channel indicating an error occured
 	 */
-	execute(message, err){
+	execute(message, err) {
 		console.log('\x1b[41m', err, '\x1b[0m');
-		
+
 		const embedTemplate = new Discord.MessageEmbed();
-		
-		embedTemplate.setColor("#b00c00");
-		embedTemplate.setTitle("yeah nah");
-		embedTemplate.setAuthor(message.client.user.username,message.client.user.avatarURL());
-		embedTemplate.setDescription("that dun broke");		
-		
-		
-		return message.channel.send({ embeds: [embedTemplate] });
-	}
+
+		embedTemplate.setColor('#b00c00');
+		embedTemplate.setTitle('yeah nah');
+		embedTemplate.setAuthor(message.client.user.username, message.client.user.avatarURL());
+		embedTemplate.setDescription('that dun broke');
+
+
+		return message.channel.send({ embed: [embedTemplate] });
+	},
 };
